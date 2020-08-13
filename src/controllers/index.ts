556121169d719310f582be7d2d2590c59f85b9d9
Adrian Furman <fadikk367@gmail.com>
2020-08-13 23:53:16 +0200

    Add index.ts in controllers dir for handy imports

---
+++ src/controllers/index.ts
@@ -0,0 +1,4 @@
+export * from './example.controller';
+export * as loginController from './login.controller';
+export * as registerController from './register.controller';
+export * as servicesController from './services.controller';
