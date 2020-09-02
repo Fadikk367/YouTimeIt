61bebd13fb70c328e7b31f3a1d2b18cc4e1070fb
Adrian Furman <fadikk367@gmail.com>
2020-09-02 18:52:05 +0200

    Move businessIds comparation to authUser middleware

--- src/middlewares/authBusiness.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { Unauthorized } from 'http-errors';
-
-export const authBusiness = async (req: Request, res: Response, next: NextFunction) => {
-  const businessId = req.params.businessId;
-  if (req.user && !req.user.businessId.equals(businessId))
-    throw new Unauthorized('This user is not registered for this business');
-  next();
-}
--- src/middlewares/authUser.ts
+++ src/middlewares/authUser.ts
@@ -40,53 +40,8 @@ const handleUserAuthentification = async (req: Request, blueprint: TokenPayload)
     throw new Unauthorized();
   }
 
-  console.log(user.businessId, req.params);
-  if (!user.businessId.equals( req.params.businessId))
+  if (req.params.businessId && !user.businessId.equals(req.params.businessId))
     throw new BadRequest();
 
   req.user = user;
 }
-
-
-
-// const handleUserRequest = async (req: Request, blueprint: TokenPayload): Promise<void> => {
-//   const user = await User.findOne({ _id: blueprint._id });
-//   if (!user) {
-//     throw new Error('User with given Id does not exist');
-//   }
-
-//   req.auth = {
-//     id: user._id,
-//     role: user.role,
-//     parentId: user._id
-//   }
-// }
-
-
-// const handleClientRequest = async (req: Request, blueprint: TokenPayload): Promise<void> => {
-//   const client = await Client.findOne({ _id: blueprint._id });
-//   if (!client) {
-//     throw new Error('Client with given Id does not exist');
-//   }
-
-//   req.auth = {
-//     id: client._id,
-//     role: client.role,
-//     parentId: client.parentId
-//   }
-// }
-
-// const handleUnregisteredRequest = async (req: Request): Promise<void> => {
-//   const parentId = req.header('parent-id');
-//   const user = await User.findOne({ _id: parentId });
-  
-//   if (!user) {
-//     throw new Error('ParentId has not been provided in a "parent-id" header for an unregistered user');
-//   }
-
-//   req.auth = {
-//     id: null,
-//     role: Role.GUEST,
-//     parentId: user._id
-//   }
-// }
